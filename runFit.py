097fcfb77bc00a13ccb8e5bdfff54a6c9c2445c1
Steffen <steffen.albrecht@desy.de>
2019-08-08 20:38:20 +0200

    cleanup and a few comments

--- runFit.py
+++ runFit.py
@@ -1,43 +1,13 @@
 #!/usr/bin/env python
-
-import os,subprocess,sys,threading
-
-class ProgressBar(threading.Timer):
-    def run(self):
-        while True:
-            self.finished.wait(self.interval)
-            if self.finished.is_set():
-                return
-            else:
-                self.function(*self.args, **self.kwargs)
-
-def update_progress(iteration,complete):
-    barLength = 30
-    status = ""
-    progress=float(iteration)/float(complete)
-    if isinstance(progress, int):
-        progress = float(progress)
-    if not isinstance(progress, float):
-        progress = 0
-        status = "error: progress var must be float\r\n"
-    if progress < 0:
-        progress = 0
-        status = "Halt...\r\n"
-    if progress >= 1:
-        progress = 1
-        status = "Done...\r\n"
-    block = int(round(barLength*progress))
-    text = "\rFits processed: %i/%i [%s] %s"%(int(iteration),int(complete),"#"*block+"-"*(barLength-block),status)
-    sys.stdout.write(text)
-    sys.stdout.flush()
-
+from __future__ import print_function
+import os,subprocess,sys
 
 def write_wrapper(dir,pathCMSSW):
     absdir=os.path.abspath(dir)
+    #create wrapper script to setup environment, combine datacards and run the fit in combine
     with open(dir+'/wrapper.sh','w') as wrapper:
         wrapper.write("""
         #!/bin/bash
-        #module use -a /afs/desy.de/group/cms/modulefiles/; module load cmssw
         source /cvmfs/cms.cern.ch/cmsset_default.sh
         cd """+pathCMSSW+"""/src/
         eval `scramv1 runtime -sh`
@@ -53,17 +23,15 @@ def runFits(models=['UHH_Model'],pathCMSSW='../CMSSW_8_1_0'):
     finishedProcesses=[]
     for model in models:
         if(not os.path.exists(model)):
-            print('Model directory %s does not exists. skipping..')
+            print('Model directory %s does not exists. skipping..'%model)
             continue
         write_wrapper(model,pathCMSSW)
+        #execute wrapper in new environment
         command=['env','-i','bash',model+'/wrapper.sh']
         fitProcesses.append(subprocess.Popen(command, shell=None,stdout=open(model+'/log.o','w'),stderr=open(model+'/log.e','w')))
+        print('Fitting', model,'...')
 
-    bar=ProgressBar(0.1,update_progress,args=(len(finishedProcesses),len(fitProcesses)))
-    bar.daemon=True
-    bar.start()
     finishedProcesses=[p.wait() for p in fitProcesses]
-    bar.cancel()
 
 
 if __name__ == "__main__":
--- uhh_producer.py
+++ uhh_producer.py
@@ -46,11 +46,8 @@ def uhh_producer(configs=None):
     #QCD ESTIMATION
     ptbins = np.array([500, 550, 600, 675, 800, 1200])
     npt = len(ptbins) - 1
-    # msdbins = np.linspace(40, 201, 24)
-    # msdbins = np.linspace(50, 180, 14)
     msdbins = np.linspace(50, 170, 21)
     nmsd = len(msdbins) - 1
-    print(msdbins)
 
     tf = rl.BernsteinPoly("qcd_pass_ralhpTF",(3,3),['pt','rho'])
     # here we derive these all at once with 2D array
@@ -73,7 +70,6 @@ def uhh_producer(configs=None):
         print(channelName)
         RebinMSD='W' in channelName
         # RebinMSD=True
-        # RebinMSD=False
         histLocation=config['histLocation']
         if('variable' in config):
             Variable=config['variable']
@@ -91,6 +87,7 @@ def uhh_producer(configs=None):
             ch=rl.Channel(regionName)
             model.addChannel(ch)
             for sName in Samples:
+                #don't include QCD Templates, but instead use rhalphabet method for QCD Estimation
                 if('QCD' in sName):
                     continue
                 sampleType=rl.Sample.SIGNAL if sName==config['signal'] else rl.Sample.BACKGROUND
@@ -98,7 +95,6 @@ def uhh_producer(configs=None):
                 hist=sFile.Get(histDir+'/'+Variable+'_central')
                 if(RebinMSD):
                     hist=hist.Rebin(len(msdbins)-1,hist.GetName()+'_newBinning',msdbins)
-                    # hist=hist.Rebin(len(msdbins)-1,"",msdbins)
                 sample=rl.TemplateSample(ch.name+'_'+sName,sampleType,hist)
                 for (gridNuisance,x,y,category) in gridNuisances:
                     histUp=sFile.Get('%s/%s_%i_%i_%s_up'%(histDir,Variable,x,y,category))
@@ -106,8 +102,6 @@ def uhh_producer(configs=None):
                     if(RebinMSD):
                         histUp=histUp.Rebin(len(msdbins)-1,histUp.GetName()+'_newBinning',msdbins)
                         histDown=histDown.Rebin(len(msdbins)-1,histDown.GetName()+'_newBinning',msdbins)
-                        # histUp=histUp.Rebin(len(msdbins)-1,"",msdbins)
-                        # histDown=histDown.Rebin(len(msdbins)-1,"",msdbins)
                     sample.setParamEffect(gridNuisance,histUp,histDown)
                     sample.setParamEffect(lumi, 1.027)
                 ch.addSample(sample)
@@ -120,16 +114,14 @@ def uhh_producer(configs=None):
                 dataHist=dataFile.Get(histPath)
                 dataHist.SetName('Mass_central')
             else:
+                #necessary for 2016 JetHT UHH Ntuples because they are missing JetConstituents
                 if('W' in channelName and config['obs']=="Data"):
                     dataHistDir=histDir.split('pt')[0]+'noConst_pt'+histDir.split('pt')[1]
                 else:
                     dataHistDir=histDir
-
                 dataHist=dataFile.Get(dataHistDir+'/'+'Mass_central')
-
             if(RebinMSD):
                 dataHist=dataHist.Rebin(len(msdbins)-1,dataHist.GetName()+'_newBinning',msdbins)
-                # dataHist=dataHist.Rebin(len(msdbins)-1,"",msdbins)
 
             ch.setObservation(dataHist)
 
@@ -147,24 +139,22 @@ def uhh_producer(configs=None):
             initial_qcd -= sample.getExpectation(nominal=True)
         if np.any(initial_qcd < 0.):
             raise ValueError("uh-oh")
-        # scaledparams = initial_qcd * (1 + 1./np.maximum(1., np.sqrt(initial_qcd)))**qcdparams
         scaledparams = initial_qcd  + 2.*np.sqrt(initial_qcd)*qcdparams
-
-        print(scaledparams[0].formula(True))
         fail_qcd = rl.ParametericSample('%sfail_qcd' % channelName, rl.Sample.BACKGROUND, obs, scaledparams)
         failCh.addSample(fail_qcd)
         pass_qcd = rl.TransferFactorSample('%spass_qcd' % channelName, rl.Sample.BACKGROUND, tf_params[ptbin, :], fail_qcd)
         model[channelName+'pass'].addSample(pass_qcd)
 
-    print("ROOT used? ", 'ROOT' in sys.modules)
     model.renderCombine(ModelName)
-    print("ROOT used? ", 'ROOT' in sys.modules)
-
 
 if __name__ == '__main__':
     import json
+    #load configuration from JSON-File
     configs=json.load(open(sys.argv[1]))
+    #create datacards and workspace
     uhh_producer(configs)
+
+    #run Fit in new environment
     from runFit import runFits
     if('pathCMSSW' in configs):
         runFits([configs['ModelName']],configs['pathCMSSW'])
