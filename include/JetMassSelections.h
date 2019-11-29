765fe74ef2edc6b32e641b5fb0a58ebdaa208ece
Dennis Schwarz <dennnis.schwarz@desy.de>
2019-11-29 15:12:48 +0100

    added 2D and MET selection

--- include/JetMassSelections.h
+++ include/JetMassSelections.h
@@ -2,10 +2,35 @@
 
 #include "UHH2/core/include/fwd.h"
 #include "UHH2/core/include/Selection.h"
+#include "UHH2/core/include/Utils.h"
+#include "UHH2/common/include/NSelections.h"
+#include "UHH2/common/include/ObjectIdUtils.h"
+#include "UHH2/common/include/JetIds.h"
+#include "UHH2/common/include/TopJetIds.h"
+#include "UHH2/common/include/Utils.h"
 #include <vector>
+
 
 class MassCut: public uhh2::Selection {
 public:
     MassCut();
     virtual bool passes(const uhh2::Event & event) override;
+};
+
+class TwoDCut : public uhh2::Selection {
+ public:
+  explicit TwoDCut(float min_deltaR_, float min_pTrel_): min_deltaR(min_deltaR_), min_pTrel(min_pTrel_) {}
+  virtual bool passes(const uhh2::Event & event) override;
+
+ private:
+  float min_deltaR, min_pTrel;
+};
+
+class METCut : public uhh2::Selection {
+ public:
+  explicit METCut(float minMet_, float maxMet_=infinity);
+  virtual bool passes(const uhh2::Event & event) override;
+
+ private:
+  float minMet, maxMet;
 };
--- src/JetMassSelections.cxx
+++ src/JetMassSelections.cxx
@@ -24,3 +24,23 @@ bool MassCut::passes(const Event & event){
   if(leading.M() > jet_lepton.M()) return true;
   else return false;
 }
+
+bool TwoDCut::passes(const Event & event){
+  if(!event.muons->size() && !event.electrons->size()) return false;
+  if(!event.jets->size()) return false;
+  float drmin, ptrel;
+  if(event.muons->size()) std::tie(drmin, ptrel) = drmin_pTrel(event.muons->at(0), *event.jets);
+  else std::tie(drmin, ptrel) = drmin_pTrel(event.electrons->at(0), *event.jets);
+  return (drmin > min_deltaR) || (ptrel > min_pTrel);
+}
+
+METCut::METCut(float minMet_, float maxMet_):
+minMet(minMet_), maxMet(maxMet_) {}
+
+bool METCut::passes(const Event & event){
+
+  assert(event.met);
+
+  float MET = event.met->pt();
+  return (MET > minMet) && (MET < maxMet);
+}
