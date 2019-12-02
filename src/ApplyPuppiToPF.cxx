36226e69e1762a23116f74d3e5595362d0574885
Steffen <steffen.albrecht@desy.de>
2019-12-02 14:45:11 +0100

    now also CorrectParticles scales the XYZE LorentzVector instead of the PtEtaPhi one

--- src/ApplyPuppiToPF.cxx
+++ src/ApplyPuppiToPF.cxx
@@ -13,9 +13,9 @@ bool ApplyPuppiToPF::process(uhh2::Event & event){
 	for (unsigned int i=0; i<particles->size();i++) {
 		//applying Puppi-Weight to LorentzVectorXYZE since the normal LorentzVector(PtEtaPhi) is behaving
 		//strange when it is multiplied by 0.
-		LorentzVectorXYZE v4XYZ = toXYZ(particles->at(i).v4());
+		LorentzVectorXYZE old_v4XYZ = toXYZ(particles->at(i).v4());
     double puppiWeight = particles->at(i).puppiWeight();
-		particles->at(i).set_v4(toPtEtaPhi(v4XYZ * puppiWeight));
+		particles->at(i).set_v4(toPtEtaPhi(old_v4XYZ * puppiWeight));
 	}
   return true;
 }
