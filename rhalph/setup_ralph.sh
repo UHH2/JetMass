6453ed45c070f849d0ba52ca67ab08c14980598d
Steffen <steffen.albrecht@desy.de>
2019-12-12 14:06:04 +0100

    setup script now installs CMSSW with combined tool if not CMSSW_10_2_13 does not exist yet

--- rhalph/setup_ralph.sh
+++ rhalph/setup_ralph.sh
@@ -5,6 +5,26 @@ if [ ! -d "rhalphalib" ]; then
   git clone https://github.com/nsmith-/rhalphalib
 fi
 
+if [ ! -d "CMSSW_10_2_13" ]; then
+	echo 'There is no CMSSW_10_2_13 installation in this directory'
+	echo 'setting up CMSSW_10_2_13 and combine'
+	export SCRAM_ARCH=slc6_amd64_gcc700
+	cmsrel CMSSW_10_2_13
+	cd CMSSW_10_2_13/src
+
+	cmsenv
+
+	git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
+	cd HiggsAnalysis/CombinedLimit
+	git fetch origin
+	git checkout v8.0.1
+	scramv1 b clean; scramv1 b
+	cd ../../../../
+fi
+
+
+bash -c ls
+
 if uname -r | grep -q el6; then
   source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-slc6-gcc8-opt/setup.sh
 else
