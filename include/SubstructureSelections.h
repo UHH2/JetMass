a1d6c5519b250377e6c60db3aded4cbf19544fd7
Steffen <steffen.albrecht@desy.de>
2020-02-05 09:20:46 +0100

    added possibility to use Mass of v4 sum of pfparticles instead of jetmass in N2ddtSelection.

--- include/SubstructureSelections.h
+++ include/SubstructureSelections.h
@@ -6,18 +6,20 @@
 #include "UHH2/core/include/Selection.h"
 #include "TH2F.h"
 #include "UHH2/core/include/TopJet.h"
+#include "UHH2/core/include/PFParticle.h"
 
 namespace uhh2 {
-  double computeDDTValue(TopJet ak8jet,TH2F* ddtMap,bool useRho=true);
+  double computeDDTValue(TopJet ak8jet,TH2F* ddtMap,bool useRho=true,std::vector<PFParticle> particles={});
   double computeTau21DDT(TopJet ak8jet, double slope);
 
   class N2ddtSelection: public uhh2::Selection {
   	public:
-  		N2ddtSelection(TH2F* ddtMap,bool useRho=false);
+    N2ddtSelection(TH2F* ddtMap,bool useRho=false,bool usePFMass=false);
   		virtual bool passes(const uhh2::Event &event) override;
   	private:
       TH2F* ddtMap;
       bool useRho;
+      bool usePFMass;
   	};
     
   class N2Selection: public uhh2::Selection {
