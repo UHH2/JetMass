d76b1b2c7d4caf10514c5ea095054428bd65e85a
Steffen <steffen.albrecht@desy.de>
2020-02-11 15:28:49 +0100

    added __init__.py for rhalphs plotter module

---
+++ rhalph/plotter/__init__.py
@@ -0,0 +1,10 @@
+from .plot_qcd_bernstein import (
+    plot_qcd_bernstein,
+)
+from .plot_fit_result import (
+    plot_fit_result,
+)
+__all__ = [
+    'plot_qcd_bernstein',
+    'plot_fit_result',
+]
