b59e76c2f3c2fd06958b3ced042b0515fc9daaee
Steffen <steffen.albrecht@desy.de>
2020-04-29 11:21:41 +0200

    added better rebinning

--- rhalph/jetmass.py
+++ rhalph/jetmass.py
@@ -15,14 +15,16 @@ def jet_mass_producer(configs=None):
       -> channels: dict with dict for each channels:
         -> includes histLocation,histDir,samples,NormUnc,signal,obs,regions    
     """
-    #specify how many bins you want the softdropmass hists to be rebinned to. (-1: no rebinning)
-    # rebin_msd = 76 # 50+75*2 GeV [50,200]
-    # rebin_msd = 13 # 50+12*12GeV [50,194]
-    # rebin_msd = 26 # 50+25*6 GeV [50,200]
-    rebin_msd = 38 # 50+37*4 GeV [50,198]
-    n_msd_bins = rebin_msd if rebin_msd > 0 else 75
-    # msd_bins = np.linspace(50,170,n_msd_bins)
-    msd_bins = np.linspace(50,int(150.//(n_msd_bins-1))*(n_msd_bins-1)+50,n_msd_bins)
+    rebin_msd = True
+    # min_msd, max_msd = (50,210)
+    # binwidth = 16
+    # nbins = int(np.floor((max_msd-min_msd)/binwidth))
+    # msd_bins = np.linspace(min_msd, nbins*binwidth+min_msd, nbins+1)
+    min_msd, max_msd = (50,190)
+    binwidth = 4
+    nbins = int(np.floor((max_msd-min_msd)/binwidth))
+    print(nbins)
+    msd_bins = np.linspace(min_msd, nbins*binwidth+min_msd, nbins+1)
     print(msd_bins)
     
     #channels for combined fit
