2295ff85bae6290ccc8b337f01e828d36e64853e
Steffen <steffen.albrecht@desy.de>
2020-10-08 16:35:59 +0200

    improved MatchingSelection (now includes different merged categories for ttbar samples)

--- include/MatchingSelections.h
+++ include/MatchingSelections.h
@@ -4,15 +4,18 @@
 #include "UHH2/core/include/Selection.h"
 #include "UHH2/core/include/TopJet.h"
 #include "UHH2/common/include/Utils.h"
+#include "UHH2/common/include/TTbarGen.h"
 
 #include <vector>
+#include "boost/algorithm/string.hpp"
 namespace uhh2{
 
 class MatchingSelection: public uhh2::Selection {
 public:
-  enum matchingOpt{oIsAnyGenW,oIsLeadingGenW,oIsMergedGenW,oIsAnyGenZ,oIsLeadingGenZ,oIsMergedGenZ};
+  // enum matchingOpt{oIsAnyGenW,oIsLeadingGenW,oIsMergedGenW,oIsAnyGenZ,oIsLeadingGenZ,oIsMergedGenZ,oIsMergedW,};
+  enum matchingOpt{oIsMergedV,oIsMergedW,oIsMergedZ,oIsMergedQB,oIsMergedTop,oIsSemiMergedTop,oIsNotMerged};
   
-  explicit MatchingSelection(uhh2::Context & ctx, matchingOpt opt);
+  explicit MatchingSelection(uhh2::Context & ctx);
   
   //this will not be used since we want to pass a (top)jet to be probed.
   //In order to ommit the warning that 'event' is not used we print this useless warning.
@@ -20,10 +23,14 @@ public:
     std::cout << "MatchingSelection: Using empty passes function on event "<< event.event << ". Are you sure you want to do that?" << std::endl;
     return true;
   };
-  bool passes_matching(const uhh2::Event & event, const TopJet &probe_jet);
+
+  void init(const uhh2::Event & event);
+  
+  bool passes_matching(const TopJet &probe_jet, matchingOpt opt, float radius = 0.8);
   
 private:
-  matchingOpt opt;
+  bool initialzed, is_VJets, is_TTbar,is_valid;
+  GenParticle genV,genTop,genB,genQ1,genQ2;
 };
 
 }
