612a96d80624da293e036e657c4b8090258bcb0d
Steffen <steffen.albrecht@desy.de>
2021-02-03 15:58:49 +0100

    changed size of massScale variation for template creation

--- include/WriteOutput.h
+++ include/WriteOutput.h
@@ -72,7 +72,7 @@ private:
   int Nbins_pt, Nbins_eta, Nbins_cat;
   vector<TString> categories;
   vector<int> otherIDs;
-  double variation = 0.1;
+  double variation = 0.01;
   std::unique_ptr<MatchingSelection> matching_selection;
   std::unique_ptr<StandaloneTopJetCorrector> softdrop_jec;
   bool isMC, do_genStudies, is_WSample, is_ZSample, isTopSel, isWSel;
