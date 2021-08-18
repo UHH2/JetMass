aeff7bebf10c34517e76b32547c2fa5730fdf0e2
Steffen <steffen.albrecht@desy.de>
2021-08-18 14:18:58 +0200

    adding new line to setup newer version of python, but disabled it, since we won't use this python anymore and rather use the CMSSW environment used for combine

--- rhalph/setup_ralph.sh
+++ rhalph/setup_ralph.sh
@@ -35,4 +35,5 @@ if uname -r | grep -q el6; then
   source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-slc6-gcc8-opt/setup.sh
 else
   source /cvmfs/sft.cern.ch/lcg/views/LCG_95apython3/x86_64-centos7-gcc8-opt/setup.sh
+  # source /cvmfs/sft.cern.ch/lcg/views/LCG_96python3/x86_64-centos7-gcc8-opt/setup.sh
 fi
