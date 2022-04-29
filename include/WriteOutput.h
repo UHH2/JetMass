93b763a975f60cff0511ae3d793b1c08b899f942
Steffen <steffen.albrecht@desy.de>
2022-04-29 14:34:25 +0200

    some fixes and adding stuff for JMS-JES correlation study

--- include/WriteOutput.h
+++ include/WriteOutput.h
@@ -6,6 +6,8 @@
 #include "UHH2/core/include/PFParticle.h"
 #include "UHH2/core/include/AnalysisModule.h"
 #include "UHH2/JetMass/include/MatchingSelections.h"
+#include "UHH2/JetMass/include/JetMassUtils.h"
+
 #include "TopJetCorrections.h"
 #include <vector>
 #include <TString.h>
@@ -21,7 +23,7 @@ using namespace uhh2;
 
 class WriteOutput: public uhh2::AnalysisModule {
 public:
-  WriteOutput(uhh2::Context & ctx, const std::string & matching_selection_handlename);
+  WriteOutput(uhh2::Context & ctx, const std::string & matching_selection_handlename, const std::string & reco_topjet_handlename, const std::string & reco_topjet_chs_handlename, const std::string & gen_topjet_handlename );
     virtual bool process(uhh2::Event & ) override;
 
 private:
@@ -29,11 +31,23 @@ private:
   double CalculateMJet(vector<PFParticle> Particles);
   vector<double> CalculateMJetVariation(vector<PFParticle>particles, int i, int j, TString cat);
   // vector<double> CalculateMJetResolutionVariation(vector<PFParticle>particles, int i, int j, TString cat);
+  std::vector<LorentzVector> variedJets(std::vector<PFParticle> particles, int i, int j, TString cat, bool apply_puppi=true);
+  std::vector<double> CalculateMJetVariation1(std::vector<PFParticle> particles, int i, int j, TString cat, bool apply_puppi=true);
+  std::vector<double> CalculatePtVariation(std::vector<PFParticle> particles, int i, int j, TString cat, bool apply_puppi=true);
+  LorentzVector nominalJet(std::vector<PFParticle> particles, bool apply_puppi=true);
+
   void ConstructOtherIDs();
   bool inCategory(PFParticle p, TString cat);
 
   int countBJetsAroundJet(const uhh2::Event & event, TopJet AK8, std::vector<Jet> AK4_jets, JetId btag = DeepJetBTag(DeepJetBTag::WP_MEDIUM), float deltaR_min = 0.8 );
   
+  uhh2::Event::Handle<int>h_n_pv;
+  uhh2::Event::Handle<int>h_n_trueint_intime;
+  uhh2::Event::Handle<int>h_n_trueint_ootimebefore;
+  uhh2::Event::Handle<int>h_n_trueint_ootimeafter;
+  uhh2::Event::Handle<float>h_n_trueint;
+
+
   uhh2::Event::Handle<double>h_msubjets; // mass of summed v4 of subjets
   uhh2::Event::Handle<double>h_mgensubjets; // mass of summed v4 of subjets
   uhh2::Event::Handle<double>h_mgenparticles; // mass of summed v4 of genparticle Candidate from subjets
@@ -78,6 +92,7 @@ private:
   uhh2::Event::Handle<double>h_CHF;
   uhh2::Event::Handle<double>h_NHF;
   
+  uhh2::Event::Handle<bool>h_IsMergedHiggs;
   uhh2::Event::Handle<bool>h_IsMergedTop;
   uhh2::Event::Handle<bool>h_IsMergedQB;
   uhh2::Event::Handle<bool>h_IsMergedWZ;
@@ -85,6 +100,11 @@ private:
 
   uhh2::Event::Handle<int>h_pdgId_Q1;
   uhh2::Event::Handle<int>h_pdgId_Q2;  
+
+  uhh2::Event::Handle<int>h_n_ak8_reco;
+  uhh2::Event::Handle<int>h_n_ak8_gen;  
+  
+
   
   std::vector<std::vector<std::vector< uhh2::Event::Handle<std::vector<double>> >>> h_jetmass_variations;
   TH2F* grid;
@@ -93,8 +113,47 @@ private:
   vector<int> otherIDs;
   double variation = 0.005;
   std::unique_ptr<MatchingSelection> matching_selection;
-  std::unique_ptr<StandaloneTopJetCorrector> softdrop_jec;
+  std::unique_ptr<StandaloneTopJetCorrector> softdrop_jec,softdrop_jec_chs;
   bool isMC, do_genStudies, is_WSample, is_ZSample, isTTbarSel, isVJetsSel;
+
+  uhh2::Event::Handle<const GenTopJet*> handle_gentopjet;
+  uhh2::Event::Handle<const TopJet*> handle_recotopjet,handle_recotopjet_chs;
+
+  uhh2::Event::Handle<double> h_dR_chs_puppi, h_dR_gen_puppi, h_dR_gen_chs;
+
+  uhh2::Event::Handle<double> h_jecfactor_puppi, h_jecfactor_chs;
+  uhh2::Event::Handle<double> h_jecfactor_puppi_sd, h_jecfactor_chs_sd;
+  
+  uhh2::Event::Handle<double> h_pt_gen_ak8, h_mass_gen_ak8, h_msd_gen_ak8;
+
+  uhh2::Event::Handle<double> h_pt_reco_ak8_puppi, h_mass_reco_ak8_puppi, h_msd_reco_ak8_puppi;
+  uhh2::Event::Handle<double> h_pt_pf_reco_ak8_puppi,h_pt_pf_sd_reco_ak8_puppi;
+
+  uhh2::Event::Handle<double> h_pt_reco_ak8_chs, h_mass_reco_ak8_chs, h_msd_reco_ak8_chs;
+  uhh2::Event::Handle<double> h_pt_pf_reco_ak8_chs,h_pt_pf_sd_reco_ak8_chs;
+
+  uhh2::Event::Handle<int> h_n_particles_puppi, h_n_particles_sd_puppi;
+  uhh2::Event::Handle<int> h_n_particles_chs, h_n_particles_sd_chs;
+
+  uhh2::Event::Handle<std::vector<int>> h_PF_multiplicities_reco_ak8_puppi,h_PF_multiplicities_reco_ak8_chs;
+  uhh2::Event::Handle<std::vector<int>> h_PF_multiplicities_reco_ak8_puppi_sd,h_PF_multiplicities_reco_ak8_chs_sd;
+
+  uhh2::Event::Handle<std::vector<float>> h_PF_energy_reco_ak8_puppi,h_PF_energy_reco_ak8_chs;
+  uhh2::Event::Handle<std::vector<float>> h_PF_energy_reco_ak8_puppi_sd,h_PF_energy_reco_ak8_chs_sd;
+
+  uhh2::Event::Handle<std::vector<float>> h_PF_pt_reco_ak8_puppi,h_PF_pt_reco_ak8_chs;
+  uhh2::Event::Handle<std::vector<float>> h_PF_pt_reco_ak8_puppi_sd,h_PF_pt_reco_ak8_chs_sd;
+ 
+  uhh2::Event::Handle<double> h_V_pt;
+  
+
+  std::vector<std::vector<std::vector< uhh2::Event::Handle<std::vector<double>> >>> h_reco_pt_variations_puppi;
+  std::vector<std::vector<std::vector< uhh2::Event::Handle<std::vector<double>> >>> h_reco_mass_variations_puppi;
+  std::vector<std::vector<std::vector< uhh2::Event::Handle<std::vector<double>> >>> h_reco_msd_variations_puppi;
+  std::vector<std::vector<std::vector< uhh2::Event::Handle<std::vector<double>> >>> h_reco_pt_variations_chs;   
+  std::vector<std::vector<std::vector< uhh2::Event::Handle<std::vector<double>> >>> h_reco_mass_variations_chs;
+  std::vector<std::vector<std::vector< uhh2::Event::Handle<std::vector<double>> >>> h_reco_msd_variations_chs;
+  
 
   /*
   Particle IDs
@@ -107,6 +166,6 @@ private:
   6 - HF tower identified as a hadron
   7 - HF tower identified as an EM particle
   */
-  vector<TString> all_cat = {"undef", "chargedH", "elec", "muon", "gamma", "neutralH", "had", "em"};
+  vector<TString> all_cat = {"undef", "chargedH", "elec", "muon", "gamma", "neutralH", "had", "em", "all"};
 
 };
