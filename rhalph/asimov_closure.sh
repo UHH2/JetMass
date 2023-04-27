18cdc59ff439522f5927b9c77d8b8d6c1a334dcc
Steffen <steffen.albrecht@desy.de>
2023-04-27 11:50:33 +0200

    adding asimov closure for unfolding where all pars are one except for one

--- rhalph/asimov_closure.sh
+++ rhalph/asimov_closure.sh
@@ -21,7 +21,7 @@ function join_by {
 
 function printexec {
   echo $1
- eval $1
+  eval $1
 }
 
 common_opt="--redefineSignalPOIs $(join_by , ${POIS[@]})"
@@ -30,9 +30,27 @@ common_opt+=" --robustFit 1"
 common_opt+=" --trackParam $(join_by , ${POIS[@]})"
 
 # perform simple fit with different values for expectSignal
-for S in 2; do
+for S in 1 1.1 2; do
   expS=$(join_by =${S}, ${POIS[@]}=${S})
   name="AsimovExpectSignal$(echo $S | sed 's/\./p/g')"
-  printexec "combine -M FitDiagnostics -d model_combined.root -t -1 --setParameters ${expS} -n .${name} ${common_opt}"
-  printexec "./../plot_pois.py -f fitDiagnostics.${name}.root -P $(join_by " " ${POIS[@]})"
+  # printexec "combine -M FitDiagnostics -d model_combined.root -t -1 --setParameters ${expS} -n .${name} ${common_opt}"
+  printexec "./../plot_pois.py -f fitDiagnostics.${name}.root -P $(join_by " " ${POIS[@]}) --expectSignal ${S}"
+done
+
+# perform fit with all POIs set to one except for one
+expS=$(join_by =1, ${POIS[@]}=1)
+# for POI in ${POIS[@]}; do
+for S in 0.9, 1.1; do
+  for POI in ${POIS[@]}; do
+    name="AsimovExpectSignal${POI}_$(echo $S | sed 's/\./p/g')"
+    printexec "combine -M FitDiagnostics -d model_combined.root -t -1 --setParameters ${expS},${POI}=${S} -n .${name} ${common_opt}"
+    printexec "./../plot_pois.py -f fitDiagnostics.${name}.root -P $(join_by " " ${POIS[@]}) --expectSignal 1.0"
+  done
+done
+
+for S in 2 -2; do
+  for POI in ${POIS[@]}; do
+    name="AsimovExpectSignal${POI}_$(echo $S | sed 's/-/n/g')"
+    printexec "./../plot_pois.py -f fitDiagnostics.${name}.root -P $(join_by " " ${POIS[@]}) --expectSignal 1.0"
+  done
 done
