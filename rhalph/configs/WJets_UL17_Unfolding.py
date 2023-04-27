afcbb887310d62569aa4dbe352fad452b31d50b8
Steffen <steffen.albrecht@desy.de>
2023-04-27 14:00:12 +0200

    update unfolding config

--- rhalph/configs/WJets_UL17_Unfolding.py
+++ rhalph/configs/WJets_UL17_Unfolding.py
@@ -1,11 +1,14 @@
 from collections import OrderedDict
+import common_configs
 
-pt_edges = [500, 575, 650, 725, 800, 1000, 1200]
+pt_edges = [500, 575, 650, 725, 800, 1000, 1200, "Inf"]
 print(len(pt_edges))
 configs = {
-    "ModelName": "VJetsUL17Unfolding",
+    "year": "UL17",
+    "ModelName": "VJetsUL17UnfoldingPrefitAsimovUniform0p825",
     "gridHistFileName": "../Histograms/grid_oneScale.root",
-    "histLocation": "../python/templates_UL17_1d_unfolding.root",
+    "histLocation": "../python/flat_templates/templates_UL17_1d_unfolding.root",
+    # "Pseudo": ["fromMC"],
     "binning": [30, 300, 5],
     "massScaleFactor": 2.0,
     "BernsteinOrders": [2, 3],
@@ -15,30 +18,18 @@ configs = {
     "QCDSigmaScale": 10.0,
     "QCDFailConstant": "False",
     "separateMassScales": "False",
-    "pt_edges": [500.0, 575.0, 650.0, 725.0, 800.0, 1000.0, 1200.0],
+    "regularization": ["msd", "pt"],
+    "regularizationStrength": 0.825,
+    "uniformGenbins": "True",
+    "scaleGenBinWidth": "False",
+    "pt_edges": [500.0, 575.0, 650.0, 725.0, 800.0, 1000.0, 1200.0, "Inf"],
+    "pt_cutoff": 1400,
+    "msd_cutoff": 100,
+    # "xsec_priors": {"WJetsMatched": 0.734689},
+    "xsec_priors": {"WJetsMatched": 0.702038},
     "unfolding_bins": {  # including overflow and underflow bin! those will be treated as background
         "ptgen": [0, 650, 800, 1200, "Inf"],
-        "msdgen": [0.0, 70., 80.5, 89.5, "Inf"],
+        "msdgen": [0., 70., 80., 90., "Inf"],
     },
-    "channels": OrderedDict(
-        [
-            (
-                "WJetsPt{}".format(pt_edges[ipt]),
-                {
-                    "QcdEstimation": "True",
-                    "selection": "W",
-                    "pt_bin": "{}to{}".format(*pt_edges[ipt: ipt + 2]),
-                    "samples": ["QCD", "WJetsMatched", "WJetsUnmatched", "ZJets", "TTToHadronic", "TTToSemiLeptonic"],
-                    "NormUnc": {
-                        # "WJets":1.2,
-                        "ZJets": 1.2
-                    },
-                    "signal": ["WJetsMatched"],
-                    "obs": "Data",
-                    "regions": ["pass", "fail"],
-                },
-            )
-            for ipt in range(len(pt_edges) - 1)
-        ]
-    ),
+    "channels": OrderedDict(common_configs.w_channels(pt_edges)),
 }
